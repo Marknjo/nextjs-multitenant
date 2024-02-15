7e15f5755d2aaee9248ba3dc08e8e9960f41346c
Mark Njoroge <marknjo7@gmail.com>
2024-02-15 17:08:36 +0300

    fix: homepage media querries styling

--- src/app/(protected)/site/page.tsx
+++ src/app/(protected)/site/page.tsx
@@ -15,7 +15,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <section className='h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col '>
+      <section className='h-full w-full md:pt-[40rem] mt-[-70px] relative flex items-center justify-center flex-col '>
         {/* grid */}
 
         <div className='absolute bottom-0 left-0 right-0 top-20 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10' />
@@ -38,7 +38,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className='flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]'>
+      <section className='flex justify-center items-center flex-col gap-4 md:!mt-[30rem] mt-[-60px]'>
         <h2 className='text-4xl text-center'>Choose what fits your right</h2>
         <p className='text-muted-foreground text-center'>
           Our straighforward pricing plans are tailored to meet your needs. If
